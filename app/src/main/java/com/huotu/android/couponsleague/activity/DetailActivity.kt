a00d4a0a61126a87414c4c6cd895ed92a51d85e6
hottech-jxd <51818549@qq.com>
2018-06-02 17:29:33 +0800

    提交代码

--- app/src/main/java/com/huotu/android/couponsleague/activity/DetailActivity.kt
+++ app/src/main/java/com/huotu/android/couponsleague/activity/DetailActivity.kt
@@ -11,7 +11,6 @@ import com.huotu.android.couponsleague.base.BaseActivity
 import com.huotu.android.couponsleague.bean.DetailBean
 import com.huotu.android.couponsleague.mvp.IPresenter
 import kotlinx.android.synthetic.main.activity_detail.*
-import kotlinx.android.synthetic.main.layout_header2.*
 
 class DetailActivity : BaseActivity<IPresenter>(),View.OnClickListener {
     var detailAdapter:DetailAdapter?=null
@@ -26,7 +25,7 @@ class DetailActivity : BaseActivity<IPresenter>(),View.OnClickListener {
     }
 
     private fun initView(){
-        header_left_image.setImageResource(R.mipmap.back2)
+
         header_left_image.setOnClickListener(this)
 
         detail_image.setImageURI("http://app.infunpw.com/commons/images/cinema/cinema_films/3823.jpg")
@@ -34,6 +33,12 @@ class DetailActivity : BaseActivity<IPresenter>(),View.OnClickListener {
         for(i in 0 .. 10){
             data.add(DetailBean(i,0,"http://app.infunpw.com/commons/images/cinema/cinema_films/3823.jpg"))
         }
+        data.add(DetailBean(4,0,"http://t00img.yangkeduo.com/t09img/images/2018-05-28/4ac0853e1a7a898315f5155bdb733dff.jpeg"))
+
+        data.add(DetailBean(4,0,"http://t04img.yangkeduo.com/images/2018-05-26/3308bf00afb37922ceef70b9991e0dfd.jpeg"))
+
+        data.add(DetailBean(4,0,"http://t08img.yangkeduo.com/images/2018-04-27/facf8f2067f128b3b2af353411c3ffcd.jpeg"))
+
 
         detail_recyclerView.layoutManager=LinearLayoutManager(this)
         detailAdapter=DetailAdapter(data)
--- app/src/main/java/com/huotu/android/couponsleague/adapter/DetailAdapter.kt
+++ app/src/main/java/com/huotu/android/couponsleague/adapter/DetailAdapter.kt
@@ -1,5 +1,7 @@
 package com.huotu.android.couponsleague.adapter
 
+import android.support.constraint.ConstraintLayout
+import android.support.design.widget.CoordinatorLayout
 import android.view.ViewGroup
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
@@ -14,7 +16,7 @@ import com.huotu.android.couponsleague.utils.FrescoDraweeListener
 class DetailAdapter(data :ArrayList<DetailBean>)
     :BaseQuickAdapter<DetailBean,BaseViewHolder>(R.layout.layout_detail_item ,  data) , FrescoDraweeListener.ImageCallback {
 
- 
+
 
     override fun convert(helper: BaseViewHolder?, item: DetailBean?) {
 
@@ -25,6 +27,6 @@ class DetailAdapter(data :ArrayList<DetailBean>)
 
     override fun imageCallback(width: Int, height: Int, simpleDraweeView: SimpleDraweeView?) {
         if(simpleDraweeView==null) return
-        simpleDraweeView.layoutParams = ViewGroup.LayoutParams(width , height)
+        simpleDraweeView.layoutParams = ConstraintLayout.LayoutParams(width , height)
     }
 }
